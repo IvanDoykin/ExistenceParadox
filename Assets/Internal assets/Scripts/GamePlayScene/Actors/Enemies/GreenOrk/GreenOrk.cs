c4d15cf9bc7d1c20a5e4397704ad41908d3dcab1
Bumbon <reelkek@gmail.com>
2020-05-23 16:13:20 +0400

    add YellowOrk.cs for example

---
+++ Assets/Internal assets/Scripts/GamePlayScene/Actors/Enemies/GreenOrk/GreenOrk.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopAdventure.Unity;
+using UnityEngine;
+
+public class GreenOrk : Actor
+{
+    [Foldout("DATA", true)] 
+    [SerializeField]
+    private DataMovement dataMovement;
+
+    [SerializeField] private DataHp dataHp;
+
+    [SerializeField] private DataDamage dataDamage;
+}
---
+++ Assets/Internal assets/Scripts/GamePlayScene/Actors/Enemies/GreenOrk/YellowOrk.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopAdventure.Unity;
+using UnityEngine;
+
+public class YellowOrk : Actor
+{
+    [Foldout("DATA", true)] [SerializeField]
+    private DataMovement dataMovement;
+
+    [SerializeField] private DataHp dataHp;
+}
