378f1ed3d4e7e976ef71203edb62b87db39d5e94
lotuz_32 <fedos_oboltus@mail.ru>
2020-08-20 10:50:06 +0300

    add gun

---
+++ Assets/Template/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    private float turnsSmoothTime = 0.2f;
+    private float turnSmoothVelocity;
+    public GameObject prefab;
+    public Transform prefab_2;
+    private void Update()
+    {
+        Vector3 oldPos = transform.rotation.eulerAngles;
+        Debug.Log(oldPos);
+        transform.LookAt(prefab_2.transform);
+        oldPos.x = transform.eulerAngles.x;
+        transform.eulerAngles = oldPos;
+
+        //Vector3 targetRotation = new Vector3(Camera.main.transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        //transform.eulerAngles = targetRotation;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+
+}
